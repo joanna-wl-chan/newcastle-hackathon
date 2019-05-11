e831c8389d867064ecfe2241a3a3558ae7fa68a9
Samuel <37462063+samuelzhang97@users.noreply.github.com>
2019-05-11 15:34:14 +1000

    Updated index profile

--- app.py
+++ app.py
@@ -2,18 +2,19 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index.html')
 def index_page():
 	return render_template('index.html');
 
-@app.route('/profile')
+@app.route('/profile.html')
 def profile_page():
 	return render_template('profile.html');
 
-@app.route('/townsville_flood')
+@app.route('/townsville_flood.html')
 def disaster_townsville():
 	return render_template('townsville_flood.html');
 
-@app.route('/donate')
+@app.route('/donate.html')
 def donate():
 	return render_template('donate.html');
 
