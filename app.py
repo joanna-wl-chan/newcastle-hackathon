e54c686ada4887516199165a6ad0565992173cf2
Samuel <37462063+samuelzhang97@users.noreply.github.com>
2019-05-11 15:39:56 +1000

    Commi

--- app.py
+++ app.py
@@ -18,5 +18,14 @@ def disaster_townsville():
 def donate():
 	return render_template('donate.html');
 
+@app.route('/success.html')
+def success():
+	return render_template('success.html')
+
+@app.route('/townsville_flood2')
+def t2():
+	return render_template('townsville_flood2.html');
+
+
 if __name__ == "__main__":
 	app.run()    
